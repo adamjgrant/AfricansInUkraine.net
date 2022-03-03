8795ffdfb005333b2ed9a4d1cb78801e8a097e1c
Adam Grant <adam_grant@apple.com>
2022-03-03 12:07:03 -0800

    Adding author information

--- components/feed_view/feed_view_acts.js
+++ components/feed_view/feed_view_acts.js
@@ -1,8 +1,14 @@
 m.feed_view.acts({
     load_in_data(_$, args) {
         const record = _$.act.get_record_by_id(args);
+        const date = ((datetime_string) => {
+            let d = new Date(new Date(datetime_string).toLocaleString("en-US", { timeZone: "Europe/Kiev" }));
+            return [d.toDateString(), d.toLocaleTimeString('en-US')].join(" ") + " Ukraine Time";
+        })(record.fields["Modified on"]);
+
         _$.me().innerHTML = `
           <h1>${record.fields.Title}</h1>
+          <header>${record.fields.Author.name}<br>${date}</header>
           <article>${converter.makeHtml(record.fields.Body)}</article>
         `;
         location.href = `#updates-${record.id}`;
