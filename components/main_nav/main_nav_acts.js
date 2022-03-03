b6d98c2b24f98a232dbf46780fe954da059d3cc9
Adam Grant <hello@adamgrant.me>
2022-03-03 07:00:45 -0800

    Pane toggling

--- components/main_nav/main_nav_acts.js
+++ components/main_nav/main_nav_acts.js
@@ -8,5 +8,19 @@ m.main_nav.acts({
     toggle_menu(_$, args) {
         const vp = document.querySelector(".viewport");
         vp.classList.toggle("show-menu");
+    },
+
+    show_pane(_$, args) {
+        const name = args.target.dataset.showPane;
+        const my_pane = document.querySelector(`[data-pane="${name}"]`);
+        _$.act.hide_all_panes();
+        my_pane.classList.remove("hide");
+    },
+
+    priv: {
+        hide_all_panes(_$, args) {
+           const panes = Array.from(document.querySelectorAll("[data-pane]"));
+           panes.forEach(pane => pane.classList.add("hide"));
+        }
     }
 })
--- components/main_nav/main_nav_events.js
+++ components/main_nav/main_nav_events.js
@@ -1,4 +1,5 @@
 m.main_nav.events(_$ => {
     _$("button.back").addEventListener("click", _$.act.go_back);
     _$("button.menu").addEventListener("click", _$.act.toggle_menu);
+    _$("[data-show-pane]").addEventListener("click", (e) => _$.act.show_pane({ target: e.target }));
 });
