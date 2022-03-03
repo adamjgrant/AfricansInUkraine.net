1c71bd9980ccf46ae1f24a8bb8a4bb25bdd44b0c
Adam Grant <hello@adamgrant.me>
2022-03-03 06:50:26 -0800

    Menu toggling works but unstyled.

--- components/main_nav/main_nav_acts.js
+++ components/main_nav/main_nav_acts.js
@@ -3,5 +3,10 @@ m.main_nav.acts({
         const vp = document.querySelector(".viewport");
         vp.classList.remove("show-feed-view");
         vp.classList.remove("show-menu-back");
+    },
+
+    toggle_menu(_$, args) {
+        const vp = document.querySelector(".viewport");
+        vp.classList.toggle("show-menu");
     }
 })
--- components/main_nav/main_nav_events.js
+++ components/main_nav/main_nav_events.js
@@ -1,3 +1,4 @@
 m.main_nav.events(_$ => {
     _$("button.back").addEventListener("click", _$.act.go_back);
+    _$("button.menu").addEventListener("click", _$.act.toggle_menu);
 });
