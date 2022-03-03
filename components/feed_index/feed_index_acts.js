89b74a2ee2e376520566196aed52ddb62e2b1bd4
Adam Grant <adam_grant@apple.com>
2022-03-03 11:56:20 -0800

    Truncating Text

--- components/feed_index/feed_index_acts.js
+++ components/feed_index/feed_index_acts.js
@@ -14,11 +14,22 @@ m.feed_index.acts({
     priv: {
         create_row_markup(_$, args) {
             const r = args.record;
+
+            const date = ((datetime_string) => {
+                let d = new Date(new Date(datetime_string).toLocaleString("en-US", { timeZone: "Europe/Kiev" }));
+                return [d.toDateString(), d.toLocaleTimeString('en-US')].join(" ") + " Ukraine Time";
+            })(r.fields["Modified on"]);
+
+            const truncated_body = ((text) => {
+                const truncation_length = 150;
+                return text.slice(0, truncation_length) + (text.length > truncation_length ? "..." : "");
+            })(converter.makeHtml(r.fields.Body));
+
             return `
               <div data-component="feed_index_item" data-feed-item-id=${r.id}>
                 <h1><a href="#updates-${r.id}">${r.fields.Title}</a></h1>
-                <article>${converter.makeHtml(r.fields.Body)}</article>
-                <footer>${r.fields.Author.name} ${r.fields["Modified on"]}</footer>
+                <article>${truncated_body}</article>
+                <footer>${r.fields.Author.name}<br>${date}</footer>
               </div>
             `;
         },
