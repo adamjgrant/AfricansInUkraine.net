347ce660668c7ebbff0ea275b1aa278e61e8595b
Adam Grant <adam_grant@apple.com>
2022-03-03 12:31:46 -0800

    Added documents page

--- components/main_nav/main_nav_events.js
+++ components/main_nav/main_nav_events.js
@@ -1,8 +1,9 @@
 m.main_nav.events(_$ => {
     _$("button.back").addEventListener("click", _$.act.go_back);
     _$("button.menu").addEventListener("click", _$.act.toggle_menu);
-    [].concat(Array.from(_$("[data-show-pane]"))).forEach(pane_trigger => {
-        console.log(pane_trigger)
+    const show_pane_buttons = [].concat(Array.from(_$("[data-show-pane]")));
+    show_pane_buttons.forEach(pane_trigger => {
         pane_trigger.addEventListener("click", (e) => _$.act.show_pane({ target: e.target }));
     })
+    _$.act.show_pane({ target: show_pane_buttons[0] });
 });
