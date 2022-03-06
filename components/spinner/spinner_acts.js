e1c2c9f42770c82aaa2fee5e38c1445046b507a6
Adam Grant <adam_grant@apple.com>
2022-03-05 17:05:08 -0800

    Switching to opalion form

--- components/spinner/spinner_acts.js
+++ components/spinner/spinner_acts.js
@@ -1,5 +1,6 @@
 m.spinner.acts({
     bind_spinner(_$, args) {
+        /*
         let iframe = document.createElement("iframe");
         iframe.classList.add("airtable-embed");
         iframe.src = "https://airtable.com/embed/shr2dXeqcnfjqvRbJ?backgroundColor=green";
@@ -8,6 +9,10 @@ m.spinner.acts({
         iframe.width = "100%";
         iframe.height = "533";
         iframe.style = "background: transparent; border: 1px solid #ccc;";
+        */
+
+        let iframe = document.createElement("iframe");
+        iframe.src = "https://docs.google.com/forms/d/15Tbnq-HijSjYEYFjEP5CCUMevY1fy3YU8V4x4_KmTbI/viewform?ts=6223a907&edit_requested=true";
         const help_frame = document.querySelector("[data-pane='help']");
 
         iframe.addEventListener("load", _$.act.hide);
